b4b94fcc44af2c777c563f3928e476489e9fbfd5
@joaogabrielz <89491372+joaogabrielz@users.noreply.github.com>
2022-06-28 15:18:01 -0400

    Delete Aula10-Consumindo-API-do-Spotify directory

--- Aula10-Consumindo-API-do-Spotify/spotlist/login.js
+++
@@ -1,8 +0,0 @@
-function login(){
-    location.href = "https://accounts.spotify.com/authorize?response_type=code&client_id=77083608262b474c97ad90d926158597&redirect_uri=http://localhost:5500/home.html&scope=playlist-modify-private playlist-modify-public playlist-read-collaborative playlist-read-private" 
-    //QueryString "?..response_type=code&client_id=770.."
-    //Retorna na URL(autorization code)
-}
-document.getElementById("btnEntrar").onclick = login;
-
-
